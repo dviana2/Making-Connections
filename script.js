465b478faed9e804ed818441445201beb12718b1
John Doe <johndoe@example.com>
2021-10-14 19:35:31 -0400

    Making Connections needs to be committed

---
+++ script.js
@@ -0,0 +1,24 @@
+console.log("JavaScript file connected");
+
+function edit() {
+    username.innerText = "Dayry Viana";
+}
+
+
+function accept(id) {
+    let user = document.querySelector(id);
+    user.remove();
+
+}
+
+function decline(id) {
+    let user = document.querySelector(id);
+    user.remove();
+}
+
+
+
+
+
+
+
